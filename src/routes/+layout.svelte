9dfbb081f2f6e484293a0925dd74771db3ae2fc7
Julien W <cefadrom1@gmail.com>
2025-10-22 21:26:50 +0200

    feat: temporary favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+    import '../app.css';
+    import faviconIco from '$lib/assets/favicon.ico';
+    import faviconPng from '$lib/assets/favicon.png';
+
+    let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" type="image/x-icon" href={faviconIco} sizes="32x32" />
+    <link rel="icon" type="image/png" href={faviconPng} sizes="128x128" />
+    <title>Scale - Agentic opportunities management</title>
 </svelte:head>
 
 {@render children?.()}
