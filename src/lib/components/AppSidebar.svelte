79068989873968dd931da0b1a9f9ffcdc3bf2910
Julien W <cefadrom1@gmail.com>
2025-10-25 19:11:58 +0200

    feat: rework sidebar spacing

--- src/lib/components/AppSidebar.svelte
+++ src/lib/components/AppSidebar.svelte
@@ -16,53 +16,62 @@
 </script>
 
 <Sidebar.Root>
-    <Sidebar.Content>
-        <Sidebar.Header>
-            <Sidebar.Menu>
-                <Sidebar.MenuItem>
-                    <Sidebar.MenuButton size="lg">
-                        {#snippet child({ props })}
-                            <a href="/" {...props}>
-                                <div class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                                    <Thermometer class="size-4" />
-                                </div>
-                                <div class="flex flex-col gap-0.5 leading-none">
-                                    <span class="font-semibold">Scale</span>
-                                </div>
-                            </a>
-                        {/snippet}
-                    </Sidebar.MenuButton>
-                </Sidebar.MenuItem>
-            </Sidebar.Menu>
-        </Sidebar.Header>
+    <Sidebar.Header>
+        <Sidebar.Menu>
+            <Sidebar.MenuItem>
+                <Sidebar.MenuButton size="lg">
+                    {#snippet child({ props })}
+                        <a href="/" {...props}>
+                            <div
+                                class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg"
+                            >
+                                <Thermometer class="size-4" />
+                            </div>
+                            <div class="flex flex-col gap-0.5 leading-none">
+                                <span class="font-semibold">Scale</span>
+                            </div>
+                        </a>
+                    {/snippet}
+                </Sidebar.MenuButton>
+            </Sidebar.MenuItem>
+        </Sidebar.Menu>
+    </Sidebar.Header>
 
-        <div class="p-2">
-            <Label class="text-xs mb-2">Search</Label>
-            <Input placeholder="Search projects..." bind:value={q} oninput={() => onSearch(q)} />
-        </div>
+    <Sidebar.Content class="gap-0">
+        <Sidebar.Group class="pb-0">
+            <Sidebar.GroupLabel>Search</Sidebar.GroupLabel>
+            <Sidebar.GroupContent>
+                <Sidebar.Menu>
+                    <Sidebar.MenuItem class="px-2">
+                        <Input placeholder="Search projects..." bind:value={q} oninput={() => onSearch(q)} />
+                    </Sidebar.MenuItem>
+                </Sidebar.Menu>
+            </Sidebar.GroupContent>
+        </Sidebar.Group>
 
-        <div class="p-2 space-y-2">
-            <Label class="text-xs">Technology</Label>
-            <Select type="single" onValueChange={(v: string) => onTechChange(v)}>
-                <SelectTrigger class="w-full">
-                    <span class="truncate text-left w-full">All technologies</span>
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectItem value="">All</SelectItem>
-                        {#each techs as t}
-                            <SelectItem value={t}>{t}</SelectItem>
-                        {/each}
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-        </div>
-
-        <Sidebar.Separator />
-
-        <Sidebar.Footer>
-            <p class="text-[10px] text-muted-foreground">Tip: use the upload button to add a new project PDF.</p>
-        </Sidebar.Footer>
+        <Sidebar.Group class="pb-0">
+            <Sidebar.GroupLabel>Technology</Sidebar.GroupLabel>
+            <Sidebar.GroupContent>
+                <Sidebar.Menu>
+                    <Sidebar.MenuItem class="px-2">
+                        <Select type="single" onValueChange={(v: string) => onTechChange(v)}>
+                            <SelectTrigger class="w-full">
+                                <span class="truncate text-left w-full">All technologies</span>
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectItem value="">All</SelectItem>
+                                    {#each techs as t}
+                                        <SelectItem value={t}>{t}</SelectItem>
+                                    {/each}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </Sidebar.MenuItem>
+                </Sidebar.Menu>
+            </Sidebar.GroupContent>
+        </Sidebar.Group>
     </Sidebar.Content>
+
     <Sidebar.Rail />
 </Sidebar.Root>
